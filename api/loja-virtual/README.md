4baf52fde4d1462023f80a51061ef76b360f35a3
Luis Felipe <luisfelipesdn12@gmail.com>
2021-08-11 23:03:33 -0300

    feat(loja-virtual): Switch to tailwind

--- api/loja-virtual/README.md
+++ api/loja-virtual/README.md
@@ -5,7 +5,9 @@ https://etec-php-exercises.vercel.app/api/loja-virtual
 
 ---
 
-[Demo Vídeo](./aula-3-demo.mp4)
+[Demo Vídeo](./tailwind-initial-demo.mp4)
+
+https://user-images.githubusercontent.com/60361387/129127195-8b91f776-1f3a-4391-ba78-210d4e9102e0.mp4
 
 https://user-images.githubusercontent.com/60361387/128925151-2b0e61d3-86cc-477e-96f1-ad88fdc564a7.mp4
 
--- api/loja-virtual/index.php
+++ api/loja-virtual/index.php
@@ -7,54 +7,104 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Loliv - Loja Virtual de Livros</title>
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <!-- Bootstrap JS-->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <!-- Tailwind CSS -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
-    <!-- JQuery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <!-- Icons by Google -->
+    <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
 </head>
 
 <body>
-    <nav class="navbar navbar-inverse" style="background-color: rgb(31, 41, 55);">
-        <div class="container-fluid">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                    <span class="sr-only">Abrir/Fechar Barra de Navegação</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#">Loliv</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                    <li><a href="#">Início</a></li>
-                    <li><a href="#">Novidades</a></li>
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mais <span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="#">Sobre nós</a></li>
-                            <li><a href="#">Kit de Imprensa</a></li>
-                            <li><a href="#">Termos</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <form class="navbar-form navbar-left" role="search">
-                    <div class="form-group">
-                        <input type="text" class="form-control" placeholder="Tartarugas até lá embaixo">
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <nav class="bg-gray-800">
+        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="relative flex items-center justify-between h-16">
+                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    <!-- Mobile menu button-->
+                    <button type="button" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <!--
+                            Icon when menu is closed.
+
+                            Heroicon name: outline/menu
+
+                            Menu open: "hidden", Menu closed: "block"
+                        -->
+                        <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                        <!--
+                            Icon when menu is open.
+
+                            Heroicon name: outline/x
+
+                            Menu open: "block", Menu closed: "hidden"
+                        -->
+                        <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                    <div class="flex-shrink-0 flex items-center">
+                        <a href="#" class="text-white font-bold text-xl">
+                            Loliv
+                        </a>
                     </div>
-                    <button type="submit" class="btn btn-default">Pesquisar</button>
-                </form>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Fale conosco</a></li>
-                    <li><a href="#"><span class="glyphicon glyphicon-log-in"> Entrar</a></li>
-                </ul>
-            </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
+                    <div class="hidden sm:block sm:ml-6">
+                        <div class="flex space-x-4 justify-center items-center">
+                            <div class="flex space-x-4 justify-center items-center">
+                                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                                <a href="#" class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium" style="height: fit-content;" aria-current="page">Início</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" style="height: fit-content;">Novidades</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" style="height: fit-content;">Sobre</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" style="height: fit-content;">Termos</a>
+
+                                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" style="height: fit-content;">Contato</a>
+                            </div>
+                            <form role="search" class="flex justify-center items-center">
+                                <input type="text" class="inline-flex justify-center py-2 px-4 border border-gray-700 shadow-sm text-sm font-medium rounded-md text-white bg-gray-900 hover:border-gray-600 focus:outline-none" placeholder="Digite o termo de pesquisa..." />
+                                <button type="submit" class="material-icons-outlined inline-flex justify-center py-2 px-3 bg-gray-800 rounded-full text-gray-400 hover:text-white">
+                                    search
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                    <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white">
+                        <a href="#" class="material-icons-outlined">
+                            login
+                        </a>
+                    </button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div class="sm:hidden" id="mobile-menu">
+            <form role="search" class="flex justify-center items-center w-full px-2">
+                <input type="text" class="w-full inline-flex justify-center py-2 px-4 border border-gray-700 shadow-sm text-sm font-medium rounded-md text-white bg-gray-900 hover:border-gray-600 focus:outline-none" placeholder="Digite o termo de pesquisa..." />
+                <button type="submit" class="material-icons-outlined inline-flex justify-center py-2 px-3 bg-gray-800 rounded-full text-gray-400 hover:text-white">
+                    search
+                </button>
+            </form>
+            <div class="px-2 pt-2 pb-3 space-y-1">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <a href="#" class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Início</a>
+
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Novidades</a>
+
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Sobre</a>
+
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Termos</a>
+
+                <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Contato</a>
+            </div>
+        </div>
     </nav>
 </body>
 
